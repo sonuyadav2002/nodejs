ca6cda2f1fad4bb7a53f62ecd7ecebbdaf1e7e3c
sonuyadav2002 <sonudonyadav87@gmail.com>
2023-07-05 09:57:59 +0530

    ss

--- From.js
+++ From.js
@@ -1,32 +1,43 @@
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 const fs = require('fs');
 
+const server = http.createServer((req, res) => {
+  const { pathname } = url.parse(req.url);
 
 
-const server = http.createServer((req,res)=>{
-    const filepath ='index.html';
-    
-    if(req.url == '/Home'){
-        fs.readFile(filepath,'utf8',(err,data)=>{
-            if(err){
-                console.log(err);
-                res.writeHead(500,{'Content-Type':'text/html'});
-                res.end('intrer server error');
-                return;
-            }
-           
-            else{
-                res.writeHead(200,{'Content-Type':'text/html'});
-                const modifiedco = data.replace('name','sonu');
-                res.end(modifiedco);
-            }
+    if(req.url === "/"){
+       fs.readFile("index.html","utf8",(err,data)=>{
+         
+
+        if(err){
+          res.writeHead(500,{ 'Content-Type': 'text/plain' });
+          res.end("internal server Error");
+          return;
+        }
+        res.writeHead(500,{ 'Content-Type': 'text/html' });
+        res.end(data);
+       })
+    }
+    else if(req.url === "/submit"){
+        let body = "";
+        req.on("data",chunk=>{
+          console.log(chunk.toString());
+          body = chunk.toString();
         })
-    }
-    else if(req.url == '/submit'){
-        res.end('data sumite')
+        //console.log(body);
+        req.on("end",()=>{
+          const formData = querystring.parse(body);
+          console.log(formData);
+           let Name = formData.name;
+           let Email = formData.email;
 
+           console.log(Name,Email);
+          
+        })
+
+
+        res.writeHead(200,{"Content-Type":"text/html"});
+        res.end("submit");
     }
-})
-server.listen(4000,()=>{
-    console.log('server open')
-})
